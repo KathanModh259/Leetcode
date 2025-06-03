79fd8683d785a4ef501a60f09a350e76ceb9d957
Kathan Modh <kathanmodh25@gmail.com>
2025-06-03 15:27:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (83.6%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else {
+                if (stk.empty()) return false;
+                if ((ch == ')' && stk.top() != '(') ||
+                    (ch == '}' && stk.top() != '{') ||
+                    (ch == ']' && stk.top() != '[')) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
