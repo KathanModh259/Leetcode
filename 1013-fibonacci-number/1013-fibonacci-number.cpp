5dbe37505d60ef202aa92681e21a9999dff63bce
Kathan Modh <kathanmodh25@gmail.com>
2025-07-06 18:08:54 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;      
-        return fib(n - 1) + fib(n - 2);
+
+        if(n == 0){
+            return 0;
+        }
+
+
+        int p2 = 1;
+        int p1 = 1;
+        int curr;
+
+
+        for(int i = 1; i < n; i++){
+            curr = p2+p1;
+            p2 = p1;
+            p1 = curr;
+        }
+
+        return p2;
+
+
     }
 };
